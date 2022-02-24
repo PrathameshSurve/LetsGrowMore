5bfa215d674506b391645f87479a2249626b5ed2
Prathamesh Sanjay Surve <91028092+PrathameshSurve@users.noreply.github.com>
2022-02-24 19:34:41 +0530

    calculator updated

--- task 3 calculator/cal.js
+++ task 3 calculator/cal.js
@@ -1,15 +1,38 @@
+var isClicked = false;
+
 // This function clear all the values
 function clearScreen() {
     document.getElementById("result").value = "";
+    isClicked = false;
 }
 
 // This function display values
 function display(value) {
-    document.getElementById("result").value += value;
+    if (value == '+' || value == '-' || value == '*' || value == '/') {
+        if (!isClicked) {
+            document.getElementById("result").value += value;
+        }
+        isClicked = true;
+    } else {
+        document.getElementById("result").value += value;
+    }
+
+
 }
 // This function evaluates the expression and return result
 function calculate() {
+    isClicked = false;
     var p = document.getElementById("result").value;
     var q = eval(p);
     document.getElementById("result").value = q;
+}
+
+
+function backSpace() {
+    var text = document.getElementById("result").value;
+    var le = text.slice((text.length - 1), text.length);
+    document.getElementById("result").value = text.slice(0, (text.length - 1));
+    if (le == '+' || le == '-' || le == '*' || le == '/') {
+        isClicked = false;
+    }
 }
